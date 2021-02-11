d7410c4d20dced7ae951cd1a1e0e58290e2e9958
Gustavo Bacellar <61608608+gustavobacellarladeira@users.noreply.github.com>
2021-02-11 18:06:27 -0300

    seidor

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from "react-router-dom";
 import './App.css';
 
-function App() {
+const App = () => {
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      <nav>
+17           <ul>
+18             <li>
+19               <Link to="/">Home</Link>
+20             </li>
+21             <li>
+22               <Link to="/products/1">First Product</Link>
+23             </li>
+24             <li>
+25               <Link to="/products/2">Second Product</Link>
+26             </li>
+27           </ul>
+28         </nav>
+      
       </header>
     </div>
   );
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const index = () => {
+    return (
+        <div>
+            <h1>
+                HOME PAGEEEEEEEEEEEE
+            </h1>
+        </div>
+    )
+}
+
+export default index
